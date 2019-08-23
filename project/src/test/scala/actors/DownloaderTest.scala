dc5ebdd06f4d790670e5ba71aeb3be0b41a8f778
pozdrowiony <aleksanderczeso@gmail.com>
2019-08-23 17:35:47 +0200

    Add tests for DownloaderTestActor

---
+++ project/src/test/scala/actors/DownloaderTest.scala
@@ -0,0 +1,32 @@
+package placeholder.actors
+
+import akka.actor._
+import akka.testkit._
+import org.scalatest._
+import java.net.{ UnknownHostException => Unknown, MalformedURLException => Malformed }
+
+class DownloadActorTest extends TestKit(ActorSystem("Test"))
+  with FlatSpecLike
+  with ImplicitSender
+  with BeforeAndAfterAll {
+
+  override def afterAll: Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  val downloadActor = system.actorOf(DownloadActor.props)
+
+  "A DownloadActor" should "send UrlDownloaded message given correct url and path" in {
+    val url = "https://jsonplaceholder.typicode.com/posts/1"
+
+    downloadActor ! DownloadActor.Download(url, ".", "a")
+        
+    expectMsg(DownloadManagerActor.UrlDownloaded(url))
+  }
+
+  it should "send Error message given wrong url or path" in {
+    downloadActor ! DownloadActor.Download("https://avemovm", ".", "a")
+
+    expectMsgType[DownloadManagerActor.Error]
+  }
+}
