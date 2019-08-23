9650f2701ea82afd871448f01f977949ebb99212
pozdrowiony <aleksanderczeso@gmail.com>
2019-08-23 18:20:55 +0200

    Implement removing files from the resources dir as the second main class

--- project/src/main/scala/Main.scala
+++ project/src/main/scala/Main.scala
@@ -2,11 +2,13 @@ import akka.actor._
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.concurrent.ExecutionContext.Implicits.global
+import java.io._
 
 import placeholder.actors._
 import placeholder.filesaver.FileSaver
 
-object Main extends App {
+// Choose this one in sbt for download the posts to resources directory
+object Download extends App {
   val ourSystem = ActorSystem("placeholder")
 
   val urlsToDownload = List.tabulate(100)(x => s"https://jsonplaceholder.typicode.com/posts/${x + 1}")
@@ -16,4 +18,11 @@ object Main extends App {
 
   // Send request periodically as the communication is unreliable
   ourSystem.scheduler.schedule(0 second, 1 second, downloadManager, DownloadManagerActor.DownloadAllUrls)
+}
+
+// Choose this one in sbt to remove saved posts from the resources directory
+object Remove extends App {
+  val d = new File("src/main/resources")
+
+  d.listFiles map (_.delete)
 }
