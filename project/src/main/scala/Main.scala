c2bdf8bed1c25863c39046b603e97ffb1cf31f77
pozdrowiony <aleksanderczeso@gmail.com>
2019-08-23 16:29:14 +0200

    Implement main - fetch 100 posts from JSONPlaceholder website

--- project/src/main/scala/Main.scala
+++ project/src/main/scala/Main.scala
@@ -1,1 +1,19 @@
-object Main extends App
+import akka.actor._
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import placeholder.actors._
+import placeholder.filesaver.FileSaver
+
+object Main extends App {
+  val ourSystem = ActorSystem("placeholder")
+
+  val urlsToDownload = List.tabulate(100)(x => s"https://jsonplaceholder.typicode.com/posts/${x + 1}")
+
+  val downloadManager: ActorRef =
+    ourSystem.actorOf(DownloadManagerActor.props(urlsToDownload, "src/main/resources"), name = "manager")
+
+  // Send request periodically as the communication is unreliable
+  ourSystem.scheduler.schedule(0 second, 1 second, downloadManager, DownloadManagerActor.DownloadAllUrls)
+}
